d6dacbc3d84430800f059db11c704f0c1c0b802a
Cosmin Stretea <49166578+Quesmin@users.noreply.github.com>
2020-12-27 18:05:32 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrplrqctE8JAfpe5afe9KPkGg_Bck1JS8",
-    authDomain: "clone-dfd68.firebaseapp.com",
-    databaseURL: "https://clone-dfd68.firebaseio.com",
-    projectId: "clone-dfd68",
-    storageBucket: "clone-dfd68.appspot.com",
-    messagingSenderId: "921104895859",
-    appId: "1:921104895859:web:2f12ee6176b084224ebf06"
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    databaseURL: DB_URL,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: SENDER_ID,
+    appId: APP_ID
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
